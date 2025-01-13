a5700e4431c863e8f4bdff5ac460dec594bc35bf
lasm <uwuboyfriendasmr@perssonrules.com>
2025-01-13 18:07:34 +0100

    structure phones in type system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+enum Phone {
+    Vowel(Vowel),
+    Consonant(Consonant),
+}
+
+impl Phone {
+    fn is_vowel(&self) -> bool {
+        match self {
+            Phone::Vowel(_) => true,
+            _ => false,
+        }
+    }
+
+    fn is_consonant(&self) -> bool {
+        match self {
+            Phone::Vowel(_) => false,
+            _ => true,
+        }
+    } 
+}
+
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
+
+struct Vowel {
+    pos: Vector2,
+    rounded: bool,
+}
+
+struct Consonant {
+    place: ArtPlace,
+    manner: Manner,
+}
+
+enum ArtPlace {
+    Bilabial,
+    Labiodental,
+    Dental,
+    Alveolar,
+    Postalveolar,
+    Velar,
+    Palatal,
+    Glottal,
+}
+
+enum Manner {
+    Plosive,
+    Fricative,
+    Nasal,
+    Glide,
+    Liquid,
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
